f5394bcee96fa9995469aced3198c5d58100d801
emad karimi <emad.k50000@gmail.com>
2025-08-13 01:00:23 +0000

    Error: In HTML, <html> cannot be a child of <body>. This will cause a hy

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,18 +6,8 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
-       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Fira+Code:wght@400;500;600&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body antialiased transition-colors duration-300 bg-background text-foreground">
-        {children}
-      </body>
-    </html>
+    <div className="dark">
+      {children}
+    </div>
   );
 }
